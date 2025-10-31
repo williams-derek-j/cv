71f4bb42e2cc60c4e5d12f731f7655c370d3b8e0
williams-derek-j <williams-derek-j@proton.me>
2025-10-31 16:29:43 -0700

    removed dynamic import
    submit button can change input to span but no idea how to preserve/transfer value of input

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,23 +1,43 @@
+import { useState, useEffect } from 'react'
 import '../styles/Personal.css'
+import Name from './Personal/Name'
+import Email from './Personal/Email'
+import Phone from './Personal/Phone'
 
 function Personal() {
-    const modules = import.meta.glob("./Personal/*.jsx", { eager: true })
+    // const modules = import.meta.glob("./Personal/*.jsx", { eager: true })
 
-    const components = {}
-    for (let i = 0; i < Object.keys(modules).length; i++) {
-        components[i] = Object.values(modules)[i].default
+    // const [components, setComponents] = useState([])
+
+    // const temp = {}
+    // for (let i = 0; i < Object.keys(modules).length; i++) {
+    //     temp[crypto.randomUUID()] = Object.values(modules)[i].default
+    // }
+
+    const [editing, setEditing] = useState(true)
+    function handleSubmit() {
+        console.log('handlesubmit ran')
+
+        setEditing(false)
     }
+
+    useEffect(() => {
+        console.log('useeffect ran')
+    }, [editing])
 
     return (
         <section id="" className="section-personal section">
-            <h1 id=">" className="header-personal header">About you:</h1>
-            <form id="" className="form-personal form">
-                {Object.entries(components).map(([key, Component]) => {
-                    return <Component key={key}/>
-                })}
-                <button id="button-submit" className="button">Submit</button>
+            <h1 id="" className="header-personal header">About you:</h1>
+            <form id="" className="form-personal form" onSubmit={(event) => {event.preventDefault()}}>
+                {/*{Object.entries(components).map(([key, Component]) => {*/}
+                {/*    return <Component key={key} editing/>*/}
+                {/*})}*/}
+                <Name editing={editing}/>
+                <Email editing={editing}/>
+                <Phone editing={editing}/>
+                <button id="" className="button-submit-personal button-submit button" onClick={() => handleSubmit()}>Submit</button>
             </form>
-            <button id="button-edit" className="button visually-hidden">Edit</button>
+            <button id="" className="button-edit-personal button-edit button visually-hidden">Edit</button>
         </section>
     )
 }
--- src/components/Personal/Email.jsx
+++ src/components/Personal/Email.jsx
@@ -1,12 +1,25 @@
-function Email() {
-    return (
-        <div id="" className="personal-email component container">
-            <label className="label">
-                Email:
-                <input name="input-email" className="input-personal-email input" type="email" placeholder="e.mail@email.com" />
-            </label>
-        </div>
-    )
+function Email(props) {
+    console.log(props)
+    if (props.editing) {
+        return (
+            <div id="" className="personal-email component container">
+                <label className="label">
+                    Email:
+                    <input name="input-email" className="input-personal-email input-email input" type="email" placeholder="e.mail@email.com" />
+                </label>
+            </div>
+        )
+    } else {
+        console.log('hi')
+        return (
+            <div id="" className="personal-email component container">
+                <label className="label-personal-email label-email label">
+                    Email:
+                    <span className="span-personal-email span-email input" content="e.mail@email.com" />
+                </label>
+            </div>
+        )
+    }
 }
 
 export default Email;
